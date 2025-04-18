f7cdc6765534ace8ccaafe017de89be9a50cc938
cloud-dev-user <70812553+cloud-dev-user@users.noreply.github.com>
2025-04-18 14:47:00 +0530

    Update WeatherApplication.java

--- src/main/java/com/weatherapp/WeatherApplication.java
+++ src/main/java/com/weatherapp/WeatherApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherApplication {
     public static void main(String[] args) {
+        System.getProperties().put("server.port", "8080"); 
         SpringApplication.run(WeatherApplication.class, args);
     }
 }
